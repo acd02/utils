959c714e33f69c93dae9151089a410c261d2f4a8
acd02 <alex.acd02@gmail.com>
2019-11-10 20:31:13 +0100

    feat: remove "doWhen" function

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import * as array from './array'
 import * as func from './function'
-import * as logic from './logic'
 import * as object from './object'
 import * as predicate from './predicate'
 
 export { array }
 export { func }
-export { logic }
 export { object }
 export { predicate }

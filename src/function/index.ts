e7da1b7057644f55a25b001abf5b23f41fb83ecc
acd02 <alex.acd02@gmail.com>
2019-11-12 15:47:33 +0100

    feat: add "result" function

--- src/function/index.ts
+++ src/function/index.ts
@@ -1,3 +1,4 @@
 export { curry } from './curry'
 export { noop, tap } from './function'
+export { result, err, ok, Result } from './result'
 export { when, whenAll } from './when'
---
+++ src/function/result.ts
@@ -0,0 +1,75 @@
+interface Err<E> {
+  readonly _tag_: 'Err'
+  readonly err: E
+}
+
+interface Ok<S> {
+  readonly _tag_: 'Ok'
+  readonly ok: S
+}
+
+export type Result<E, S> = Err<E> | Ok<S>
+
+type ResultBox<E, S> = {
+  /**
+   * Takes two functions. a first function that will get executed if the value
+   * is an `Err`, and a second function, that will get executed if the value
+   * is an `Ok`.
+   *
+   */
+  fold<U>(onErr: (a: E) => U, onSuccess: (a: S) => U): U
+}
+
+export function err<E = never, A = never>(e: E): Result<E, A> {
+  return { _tag_: 'Err', err: e }
+}
+
+export function ok<E = never, S = never>(a: S): Result<E, S> {
+  return { _tag_: 'Ok', ok: a }
+}
+
+function isErr<E, S>(a: Result<E, S>): a is Err<E> {
+  switch (a._tag_) {
+    case 'Err':
+      return true
+    case 'Ok':
+      return false
+  }
+}
+
+/**
+ * Wraps a value of one of two possible types (Result) and returns a `ResultBox` object
+ * allowing you to unfold the value to handle both cases.
+ *
+ * An instance of `Result` is either an instance of Err or Ok.
+ * The first type is used for failure (Err), the second for success (OK).
+ *
+ * @param value A Result (either an Err or OK)
+ * @returns Returns a `ResultBox` with 1 method, `fold`
+ *
+ * @example
+ *
+ * const data: Result<number, string>
+ *
+ * fetch('someapi')
+ *  .then((res: string) => setData(ok(res)))
+ *  .catch((e: number) => setData(err(e)))
+ *
+ * result(data).fold(
+ *   (e) => `the error code is ${e}`,
+ *   (s) => `the data is ${s}`
+ * )
+ *
+ */
+export function result<E, S>(a: Result<E, S>) {
+  const self = {} as ResultBox<E, S>
+
+  function fold<U>(onErr: (a: E) => U, onSuccess: (a: S) => U) {
+    if (isErr(a)) return onErr(a.err)
+    return onSuccess(a.ok)
+  }
+
+  self.fold = fold
+
+  return self
+}
